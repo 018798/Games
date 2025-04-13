e5e26c40f1c062ebb9dbd06eca1a4029c47c2573
018798 <100913028+018798@users.noreply.github.com>
2025-04-13 18:54:18 +1000

    added my plan file. html and JS

---
+++ rps.js
@@ -0,0 +1,15 @@
+
+
+
+function playgame (getHumanChoice){
+
+    
+    const choices = ["Rock","Paper","scissors"];
+    const getComputerChoice = choices(math.floor()(math.random()*choices.length));
+    let result = 
+
+  
+}
+
+console.log ("Hello World")
+
